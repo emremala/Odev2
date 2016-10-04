fce88fa0d1ac4dc2fbed476da76843704be4e715
emremala <emremala@hotmail.com>
2016-10-04 14:23:56 +0300

    Add files via upload

---
+++ Odev2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Odev2.Startup))]
+namespace Odev2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
